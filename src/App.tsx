38724aa9f37706c131d9e3d16943b65bfb53b87f
Arnav Sastry <arnavsastry@gmail.com>
2025-06-19 20:38:16 -0400

    time settings panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
-import Table from "./Table.tsx";
+import { useEffect, useState } from "react";
 
-const App = () => {
+// components
+import Table from "./Table.tsx";
+import TimeSettings from "./TimeSettings.tsx";
+
+const App = ({
+  initialTimeSettings = {
+    startTime: "05:15",
+    endTime: "09:00",
+  },
+  persistKey = "hmua-app",
+}) => {
+  const [timeSettings, setTimeSettings] = useState(initialTimeSettings);
+
+  const updateTime = (field, value) => {
+    setTimeSettings((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
+
+  const people = ["Bride", "MoB", "MoG", "Bridesmaid"];
+  const tasks = ["Hair", "Makeup", "Draping"];
+  const initialData = [
+    {
+      id: 1,
+      person: "Bride",
+      Hair: 180,
+      Makeup: 0,
+      Draping: 0,
+      deadline: "14:00",
+    },
+    {
+      id: 2,
+      person: "MOB",
+      Hair: 45,
+      Makeup: 45,
+      Draping: 30,
+      deadline: "13:30",
+    },
+    {
+      id: 3,
+      person: "MOG",
+      Hair: 45,
+      Makeup: 45,
+      Draping: 30,
+      deadline: "13:30",
+    },
+    {
+      id: 4,
+      person: "MoH",
+      Hair: 45,
+      Makeup: 45,
+      Draping: 15,
+      deadline: "13:45",
+    },
+  ];
+
   return (
     <div>
       <h1 className="text-center">HMUA Scheduler</h1>
-      <Table />
+      <TimeSettings timeSettings={timeSettings} updateTime={updateTime} />
+      <Table data={initialData} tasks={tasks} />
     </div>
   );
 };
--- src/Table.tsx
+++ src/Table.tsx
@@ -1,59 +1,10 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-const Table = () => {
-  const [tasks, _setTasks] = useState(["Hair", "Makeup", "Draping"]);
-
-  /*
-  const professionals = {
-    'Hair': 2,
-    'Makeup': 2,
-    'Draping': 1,
-  };
-  const timeSettings = {
-    startTime: '05:15',
-    endTime: '09:00'
-  };
-  */
-
-  const initialData = [
-    {
-      id: 1,
-      person: "Bride",
-      Hair: 180,
-      Makeup: 0,
-      Draping: 0,
-      deadline: "14:00",
-    },
-    {
-      id: 2,
-      person: "MOB",
-      Hair: 45,
-      Makeup: 45,
-      Draping: 30,
-      deadline: "13:30",
-    },
-    {
-      id: 3,
-      person: "MOG",
-      Hair: 45,
-      Makeup: 45,
-      Draping: 30,
-      deadline: "13:30",
-    },
-    {
-      id: 4,
-      person: "MoH",
-      Hair: 45,
-      Makeup: 45,
-      Draping: 15,
-      deadline: "13:45",
-    },
-  ];
-
+const Table = ({ data, tasks }) => {
   const colDefs = useMemo(() => {
     const cols = [
       {
@@ -94,9 +45,9 @@ const Table = () => {
   );
 
   return (
-    <div className="mx-auto w-96 h-256 text-center">
+    <div className="mx-auto w-96 h-96 text-center md:w-128">
       <AgGridReact
-        rowData={initialData}
+        rowData={data}
         columnDefs={colDefs}
         defaultColDef={defaultColDef}
         gridOptions={gridOptions}
